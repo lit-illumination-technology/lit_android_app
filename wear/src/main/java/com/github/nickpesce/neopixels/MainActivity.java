25b02d7919ea6b1d564480ace874bcdf4bbac8ee
Nick Pesce <npesce@terpmail.umd.edu>
2017-01-26 22:17:09 -0500

    Added additional files needed to build

---
+++ wear/src/main/java/com/github/nickpesce/neopixels/MainActivity.java
@@ -0,0 +1,26 @@
+package com.github.nickpesce.neopixels;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    private TextView mTextView;
+
+    //TODO data layer between phone and watch.
+    //Recv possible commands. Send commands.
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+    }
+}
